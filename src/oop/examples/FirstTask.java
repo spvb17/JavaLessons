d4cd3672e17c45aee92d64e446fce31e35cd5ab1
spvb17 <saparalievb17@gmail.com>
2022-11-03 22:43:44 +0600

    oop intro

--- src/oop/examples/FirstTask.java
+++ src/oop/examples/FirstTask.java
@@ -1,2 +1,55 @@
-package oop.examples;public class FirstTask {
+package oop.examples;
+
+public class FirstTask
+{
+    public static void main(String[] args) {
+        Student studnent1 = new Student(29656, "Samat", "Myrzatayev", 2.9);
+        Student student2 = new Student(29400, "Asan", "Usenov", 3.5);
+        Student student3 = new Student(29500, "Marat", "Muratov", 3.8);
+        Student student4 = new Student(29516, "Renat", "Ilyinov", 2.5);
+        Student student5 = new Student(29815, "Rozalina", "Amalbek", 3.9);
+        Student[] arr = {studnent1, student2, student3, student4, student5};
+//        for(int i=0; i<arr.length; ++i)
+//        {
+//            System.out.println(arr[i].getStudentData());
+//        }
+        topStudent(arr);
+    }
+
+    public static void topStudent(Student[] arr)
+    {
+        Student student = arr[0];
+        for(int i=0; i<arr.length; ++i)
+        {
+            if(arr[i].gpa > student.gpa)
+            {
+                student = arr[i];
+            }
+        }
+        System.out.println("The best student is " + student.getStudentData());
+    }
+}
+
+class Student
+{
+    public int id;
+    public String name;
+    public String surname;
+    public double gpa;
+
+    public Student(){};
+    public Student(int id, String name, String surname, double gpa)
+    {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.gpa = gpa;
+    }
+
+    public String getStudentData()
+    {
+        return "My name is " + name + " " + surname + ". My id is " + id + ", and my gpa is " + gpa;
+    }
+
+
 }
--- src/oop/examples/SecondTask.java
+++ src/oop/examples/SecondTask.java
@@ -1,2 +1,53 @@
-package oop.examples;public class SecondTask {
+package oop.examples;
+
+public class SecondTask
+{
+    public static void main(String[] args) {
+        Car car1 = new Car("Mercedes", 2005, 3.4, 50);
+        Car car2 = new Car("BMW", 2001, 4.4, 60);
+        Car car3 = new Car("Toyota", 2003, 2.0, 40);
+        Car car4 = new Car("Kia", 2009, 1.6, 55);
+        Car[] arr = {car1, car2, car3, car4};
+        oldestCar(arr);
+    }
+
+    public static void oldestCar(Car[] arr)
+    {
+        Car car = arr[0];
+        for(int i=0; i<arr.length; ++i)
+        {
+            if(arr[i].year<car.year)
+            {
+                car = arr[i];
+            }
+        }
+        System.out.println("The oldest car is " + car.name);
+    }
+}
+
+class Car
+{
+    public String name;
+    public int year;
+    public double engineVolume;
+    public int speed;
+
+    public Car(){};
+    public Car(String name, int year, double engineVolume, int speed)
+    {
+        this.name = name;
+        this.year = year;
+        this.engineVolume = engineVolume;
+        this.speed = speed;
+    }
+
+    public void acceleration()
+    {
+        speed+=10;
+    }
+
+    public void brake()
+    {
+        speed-=15;
+    }
 }
